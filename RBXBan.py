3f49b96ef65818aabf50be29247d3a7f1d44dc1c
OriginalAlien <80835991+OriginalAlien@users.noreply.github.com>
2022-08-10 00:49:08 -0400

    Create RBXBan.py

---
+++ RBXBan.py
@@ -0,0 +1,152 @@
+#Importing Modules
+import requests, json
+import random, time
+from bs4 import BeautifulSoup
+from pystyle import Colorate, Colors, Add, Center, Write
+
+c = open("cookies.txt", "r").readlines()
+u = open("useragents.txt", "r").readlines()
+p = open("proxy.txt", "r").readlines()
+
+cookies    = []
+useragents = []
+proxies    = []
+
+for i in c:
+	cookies.append(i.replace('\n', ''))
+	
+for i in u:
+	useragents.append(i.replace('\n', ''))
+
+for i in p:
+	proxies.append(i.replace('\n', ''))
+
+#For Getting Stuff
+class Utils:
+	def getProxy():
+		proxy = random.choice(proxies)
+		return proxy
+		
+	def getUserAgent():
+		useragent = random.choice(useragents)
+		return useragent
+		
+	def getCookie():
+		cookie = random.choice(cookies)
+		return cookie
+
+	def getXCsrf(cookie):
+	      xcsrfRequest = requests.post("https://auth.roblox.com/v2/logout", cookies={
+	          ".ROBLOSECURITY": cookie
+	      })
+		
+	      return xcsrfRequest.headers["x-csrf-token"]
+
+	def getRequestVerificationToken(cookie):
+		requestHTML = requests.get(
+			"https://www.roblox.com/build/upload",
+			headers = {"referer": "https://roblox.com"},
+			cookies = {".ROBLOSECURITY": cookie}
+        )
+		try:
+			soup = BeautifulSoup(requestHTML.text, "lxml")
+			verifyToken = soup.find("input", {"name" : "__RequestVerificationToken"}).attrs["value"]
+		except Exception as E:
+			Write.Print(f"\n[>] Error: {E}\n[>] Enter to Exit...", Colors.purple_to_red, interval=0.0025)
+			input()
+			exit()
+		return verifyToken
+		
+	def getOutput(amount, request, proxy, useragent):
+		if request.status_code == 200:
+			Write.Print(f"\n[{amount}] {request.status_code} | {proxy} | {useragent} | \n\n", Colors.green, interval=0.0025)
+		else:
+			Write.Print(f"\n[{amount}] {request.status_code} | {proxy} | {useragent} | \n", Colors.purple_to_red, interval=0.0025)
+
+#Style & Designs		
+def getBanner():
+	bannerText = """
+     __   __       __ 
+    |__) |__) \_/ |__)  /\  |\ |
+    |  \ |__) / \ |__) /~~\ | \|
+	
+       Roblox Mass Reporter
+         [ Dreamer#5114 ] 
+"""
+	
+	bannerLogo = """
+         ⣴⣶⣄
+       ⣴⣿⣇⡙⢿⣷⣄
+     ⣴⣿⣿⣄⠨⣍⡀⠙⣿⡇
+   ⣴⣿⣿⡈⣉⠛⢷⣌⣻⣿⠟
+ ⣴⠿⢋⣉⠻⢧⡈⢴⣦⣾⠟
+ ⢿⣷⣌⠁⣶⢌⣿⣾⠟⢡⣶⣄
+  ⠙⢿⣷⣤⣾⠟   ⠙⢿⣷⣄
+             ⠙⢿⣷⣄
+               ⠙⢿⣷⣄
+                 ⠙⢿⣷⣄
+                   ⠙⡙⣴⣦⠙
+                    ⣌⠛⢋⣴
+
+
+"""
+
+	banner = Colorate.Vertical(Colors.purple_to_blue, Center.Center(Add.Add(bannerLogo, bannerText, 0)), 1)
+	return banner
+
+#Report Function
+	
+def report(victim, amount, reason, cooldown, descriptions):
+	
+	if amount == 0:
+		amount = 999999999999999
+		
+	id = json.loads(requests.get(
+		f"https://api.roblox.com/users/get-by-username?username={victim}",
+		headers={
+			"referer": "https://www.roblox.com",
+			"User-Agent": Utils.getUserAgent()
+		},
+
+		proxies = {
+			"http": Utils.getProxy()
+		}).text)["Id"]
+
+	Write.Print(f"[>] Victim's User ID: {id}\n", Colors.purple_to_blue, interval=0.0025)
+
+	for i in range(amount):
+		time.sleep(cooldown)
+		
+		proxy     = Utils.getProxy()
+		useragent = Utils.getUserAgent()
+		cookie    = Utils.getCookie()
+		
+		reportRequest = requests.post(
+			f"https://www.roblox.com/abusereport/userprofile?id={id}",
+			data = {
+				"__RequestVerificationToken": Utils.getRequestVerificationToken(cookie),
+				"ReportCategory": reason,
+				"Comment": random.choice(descriptions),
+				"Id": id,
+				"RedirectUrl": f"https://www.roblox.com/users/{id}/profile,",
+				"PartyGuid": "",
+				"ConversationId": ""
+			},
+			
+			headers = {
+				"x-csrf-token": Utils.getXCsrf(cookie),
+				"User-Agent": useragent,
+				"referer": "https://www.roblox.com"
+			},
+
+			proxies = {
+				"http": proxy
+			},
+
+			cookies = {
+				".ROBLOSECURITY": cookie
+			}
+		)
+		
+		Utils.getOutput(i, reportRequest, proxy, useragent)
+    #gig
