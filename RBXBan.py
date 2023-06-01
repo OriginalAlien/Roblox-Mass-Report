0a9ee90f627c4940f519fcf0efc89f0af440e5f8
OriginalAlien <80835991+OriginalAlien@users.noreply.github.com>
2023-06-01 03:03:57 -0400

    Update RBXBan.py

--- RBXBan.py
+++ RBXBan.py
@@ -1,81 +1,51 @@
 #Importing Modules and Setting up proxies and useragents and cookie
-import requests, json, lxml, random, time
+import requests, json, random, time
 from bs4 import BeautifulSoup
 from pystyle import Colorate, Colors, Add, Center, Write
 validReports = 0
 
 c = open("cookies.txt", "r").readlines()
-u = open("useragents.txt", "r").readlines()
-p = open("proxy.txt", "r").readlines()
-
-cookies    = []
-useragents = []
-proxies    = []
-for i in c:
-	cookies.append(i.replace('\n', ''))
-	
-for i in u:
-	useragents.append(i.replace('\n', ''))
-
-for i in p:
-	proxies.append(i.replace('\n', ''))
+cookies = [i.replace('\n', '') for i in c]
 
 #For Getting Stuff
-class Utils:
-	def getProxy():
-		proxy = random.choice(proxies)
-		return proxy
-		
-	def getUserAgent():
-		useragent = random.choice(useragents)
-		return useragent
-		
+class Utils:		
 	def getCookie():
 		cookie = random.choice(cookies)
 		return cookie
 
-	def getXCsrf(cookie):
-	    xcsrfRequest = requests.post("https://auth.roblox.com/v2/logout",
-									 
-		headers = {"referer": "https://roblox.com", "User-Agent": Utils.getUserAgent()},
-		cookies = {".ROBLOSECURITY": cookie},
-		proxies = {"http": Utils.getProxy()})
-	    return xcsrfRequest.headers["x-csrf-token"]
-
 	def getRequestVerificationToken(cookie):
-		useragent = Utils.getUserAgent()
 		requestHTML = requests.get(
 			"https://www.roblox.com/build/upload",
-			headers = {"referer": "https://roblox.com", "User-Agent": useragent},
+			headers = {"referer": "https://roblox.com"},
 			cookies = {".ROBLOSECURITY": cookie},
-			proxies = {"http": Utils.getProxy()})
-		try:
-			soup = BeautifulSoup(requestHTML.text, "lxml")
-			verifyToken = soup.find("input", {"name" : "__RequestVerificationToken"}).attrs["value"]
-			return verifyToken
-			
-		except AttributeError as E:
-			Write.Print(f"\n\n[>] User-Agent Used: {useragent}", Colors.purple_to_red, interval=0.0025)
-			Write.Print(f"\n[>] Error: {E}\n[>] Continuing...\n", Colors.purple_to_red, interval=0.0025)
-			pass
-			return "Skipped"
+		)
+		
+		soup = BeautifulSoup(requestHTML.text, "html.parser")
+		verifyToken = soup.find("input", {"name" : "__RequestVerificationToken"}).attrs["value"]
+		return verifyToken
+		
+	def getOutput(amount, request):
 
-	def getOutput(amount, request, proxy, useragent):
-		global validReports
 		if request.status_code == 200:
-			Write.Print(f"\n[{amount}] {request.status_code} |  {proxy}{(25-(len(proxy)))*' '} |   {useragent}", Colors.green, interval=0)
+			Write.Print(f"\n[{amount}] {request.status_code} |      Report Success      | {request.reason}", Colors.green, interval=0)
+			global validReports
 			validReports += 1
+		elif request.status_code == 429:
+			Write.Print(f"\n[{amount}] {request.status_code} | Rate Limited  (Cooldown) | {request.reason}", Colors.purple_to_red, interval=0)
+			time.sleep(600) #10 Minute Cooldown (in seconds)
 		else:
-			Write.Print(f"\n[{amount}] {request.status_code} |  {proxy}{(25-(len(proxy)))*' '} |   {useragent}", Colors.purple_to_red, interval=0)
-#Style & Designs		
+			Write.Print(f"\n[{amount}] {request.status_code} |      Report  Failed      | {request.reason}", Colors.purple_to_red, interval=0)
+
+#Style & Designs
 def getBanner():
 	bannerText = """
-     __   __       __ 
-    |__) |__) \_/ |__)  /\  |\ |
-    |  \ |__) / \ |__) /~~\ | \|
-	
-       Roblox Mass Reporter
-         [ Dreamer#5114 ] 
+  __   __       __ 
+ |__) |__) \_/ |__)  /\  |\ |
+ |  \ |__) / \ |__) /~~\ | \| 
+
+     Roblox Mass Reporter 
+     1% it acutally bans
+       [ cereb#8577 ]
 """
 	
 	bannerLogo = """
@@ -100,59 +70,49 @@ def getBanner():
 
 #Report Function
 	
-def report(victim, amount, reason, cooldown, descriptions):
-	
+def ban(victim, amount, reason, cooldown, comments):
+
 	if amount == 0:
 		amount = 999999999999999
-		
-	id = json.loads(requests.get(
-		f"https://api.roblox.com/users/get-by-username?username={victim}",
-		headers={
-			"referer": "https://www.roblox.com",
-			"User-Agent": Utils.getUserAgent()
-		},
 
-		proxies = {
-			"http": Utils.getProxy()
-		}).text)["Id"]
+	id = json.loads(requests.post("https://users.roblox.com/v1/usernames/users", json={
+		"usernames": [victim],
+		"excludeBannedUsers": "true"
+	}).text)["data"][0]["id"]
 
 	Write.Print(f"[>] Victim's User ID: {id}\n", Colors.purple_to_blue, interval=0.0025)
 
 	for i in range(amount):
 		time.sleep(cooldown)
-		proxy     = Utils.getProxy()
-		useragent = Utils.getUserAgent()
-		cookie    = Utils.getCookie()
-		reportRequest = requests.post(
+		cookie = Utils.getCookie()
+		session = requests.Session()		
+
+		xcsrfToken = requests.post("https://auth.roblox.com/v2/logout", cookies={".ROBLOSECURITY": cookie}) #xcsrfToken only works with requests, not session
+		xcsrfToken = xcsrfToken.headers["x-csrf-token"]
+
+		session.cookies.update({".ROBLOSECURITY": cookie})
+		session.headers.update({"referer": "https://www.roblox.com", "x-csrf-token": xcsrfToken})
+
+		requestHTML 			 = session.get("https://www.roblox.com/build/upload")
+		soup 				     = BeautifulSoup(requestHTML.text, "html.parser")
+		requestVerificationToken = soup.find("input", {"name" : "__RequestVerificationToken"}).attrs["value"]
+
+		reportRequest = session.post(
 			f"https://www.roblox.com/abusereport/userprofile?id={id}",
 			data = {
-				"__RequestVerificationToken": Utils.getRequestVerificationToken(cookie),
+				"__RequestVerificationToken": requestVerificationToken,
 				"ReportCategory": reason,
-				"Comment": random.choice(descriptions),
+				"Comment": random.choice(comments),
 				"Id": id,
 				"RedirectUrl": f"https://www.roblox.com/users/{id}/profile,",
 				"PartyGuid": "",
 				"ConversationId": ""
-			},
-			
-			headers = {
-				"x-csrf-token": Utils.getXCsrf(cookie),
-				"User-Agent": useragent,
-				"referer": "https://www.roblox.com"
-			},
-
-			proxies = {
-				"http": proxy
-			},
-
-			cookies = {
-				".ROBLOSECURITY": cookie
 			}
-		)		
-		Utils.getOutput(i, reportRequest, proxy, useragent)
+		)
+		
+		Utils.getOutput(amount=i, request=reportRequest)
 
 	Write.Print(f"\n\n[>] Finished Mass Report.\n[>] Reports Sent: {amount}\n[>] Valid Reports: {validReports}\n\n", Colors.purple_to_blue, interval=0.0025)
 	Write.Input("[>] Enter to Exit...", Colors.purple_to_blue, interval=0.0025)
 	
 	exit()
-#gig
